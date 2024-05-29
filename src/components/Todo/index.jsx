2d97d92ccd5cc5da2f01e796f55c9625b991ae5f
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-05-29 19:46:53 +0300

    first commit

---
+++ src/components/Todo/index.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function Todo() {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state);
+
+
+  const deleted = (id) => {
+    dispatch({
+      type: "DELETE",
+      payload: id,
+    });
+  };
+  const line = (id) => {
+    dispatch({
+      type: "LINE",
+      payload: id,
+    });
+  };
+
+  const [holder, setHolder] = useState('');
+
+
+  const getValue = (e) =>{
+     setHolder(e.target.value)
+   
+  }
+  
+
+  const addTask = (e) => {
+    setHolder('');
+    e.preventDefault();
+    let newItem = {
+      text: holder,
+      completed: false
+    }
+    dispatch({
+      type: "ADDTASK",
+      payload: newItem,
+    });
+  };
+
+
+   
+      
+  return (
+    <>
+    <header>
+      <h1>list: {todos.length}</h1>
+    </header>
+    <main>
+    <div className={todos ? "todos" : ""}>
+      {todos.map((todo, id) => {
+        return (
+          <div className="todo" key={id}>
+            <input
+              type="checkbox"
+              defaultChecked={todo.completed}
+              onClick={() => line(id)}
+            />
+            <p className={todo.completed ? "strike" : ""}>{todo.text}</p>
+            <button  onClick={() => deleted(id)}>X</button>
+          </div>
+        );
+      })}
+    </div>
+    <div className="input">
+      <form   > 
+        <input type="text" placeholder="add task..." value={holder} onChange={getValue}/>
+        {/* <button type="button" onClick={addTask}>add</button> */}
+        <div className="btn" type="button" onClick={addTask}>add</div>
+      </form>
+    </div>
+    </main>
+    </>
+  );
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const defaultState = [
+  { text: 'html', completed: true },
+  { text: 'css', completed: false }
+]
+
+const reducer = (state = defaultState , action) =>{
+  console.log(action)
+
+switch (action.type) {
+     case 'LINE':
+        return [...state.map((item, id) =>{
+          if(action.payload === id){
+            return {...item, completed: !item.completed}
+          } return {...item}
+        })];
+      case 'DELETE':
+        return ([...state.filter((item, id) => {
+          if(action.payload === id){
+            return false
+          } return true
+         }) ]);
+         case 'ADDTASK':
+          if(action.payload.text ){
+            return [...state, action.payload]
+          }
+          return [...state]
+        case 'LENGHT':
+         return state
+        
+      
+    default: return state;
+}
+
+//  if(action.type === 'PLUS'){ return state + 1 };
+//  if(action.type === 'MINUS'){ return state - 1 };
+//  if(action.type === 'MINUS'){ return state = 0 }
+
+ } 
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+<>
+
+<Provider store={store}>
+ <App />
+ </Provider>
+   
+</>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// (state.filter((item, id) =>{
+//   if(action.payload === id ){
+//     return false
+//   } return true
+// }))
+
